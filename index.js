b476dda3e4647f592d0ed04c1956d54b94624b9d
Ben Oluoch <splashmaxsoln@gmail.com>
2025-03-17 20:26:43 +0300

    Group Discussion

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+let cats = [ "Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
+
+//TODO: Valentine
+function destructivelyPrependCat(name){
+    
+}
+
+//TODO: Edith
+function  destructivelyRemoveLastCat(name){
+
+}
+
+//TODO: Anthony
+function destructivelyRemoveFirstCat(name){
+
+}
+
+//TODO: Alvin
+function appendCat(name){
+
+}
+
+
+//TODO: Neema
+function prependCat(name){
+
+}
+
+console.log(prependCat("Arnold"))
+
+
+//TODO: Crispus
+function  removeLastCat(name){
+    
+}
+ function  removeFirstCat(name){
+    return cats.slice(1)
+ }
+ console.log(removeFirstCat("Arnold"))
